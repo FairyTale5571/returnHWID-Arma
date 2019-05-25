b8baf6263248c88065e1769c53b47fb026c7647d
FairyTale#5571 <onopa96@gmail.com>
2019-05-25 03:18:26 +0300

    Added: HDD id, Windows Product Id, crypted result in sha256
    Updated README

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Return hardware ID to Arma
  
  Build: go build -o returnHWID_x64.dll -buildmode=c-shared
 
- Usage in Arma: "returnHWID" callExtension "";
+ Usage in Arma:
+    "returnHWID" callExtension "Machine_ID";
+    "returnHWID" callExtension "HDD_UID";
+    "returnHWID" callExtension "Product_Win";
+    -Result: STRING Crypted in sha256
--- main.go
+++ main.go
@@ -8,20 +8,24 @@ package main
 import "C"
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
 	"unsafe"
 
 	"golang.org/x/sys/windows/registry"
 )
 
-func HardwareID() (string, error) {
-	id, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Cryptography`, registry.QUERY_VALUE|registry.WOW64_64KEY)
+const sha256Key = "cn3487tcqyb#%*^@!mdr#rwajmnwa239rjqrc34j"
+
+func readRegistr(input1 string, value string) (string, error) {
+	id, err := registry.OpenKey(registry.LOCAL_MACHINE, input1, registry.QUERY_VALUE|registry.WOW64_64KEY)
 	if err != nil {
 		return "", err
 	}
 	defer id.Close()
 
-	s, _, err := id.GetStringValue("MachineGuid")
+	s, _, err := id.GetStringValue(value)
 	if err != nil {
 		return "", err
 	}
@@ -39,10 +43,36 @@ func RVExtensionVersion(output *C.char, outputsize C.size_t) {
 	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
 }
 
+func protect(appID, id string) string {
+	mac := hmac.New(sha256.New, []byte(id))
+	mac.Write([]byte(appID))
+	return fmt.Sprintf("%x", mac.Sum(nil))
+}
+
+func ReturnMyData(input *C.char) string {
+	in := C.GoString(input)
+	rID := ""
+	switch in {
+	case "Machine_ID":
+		id, _ := readRegistr(`SOFTWARE\Microsoft\Cryptography`, "MachineGuid")
+		rID = fmt.Sprintf(protect(sha256Key, id))
+	case "HDD_UID":
+		id, _ := readRegistr(`HARDWARE\DESCRIPTION\System\MultifunctionAdapter\0\DiskController\0\DiskPeripheral\0`, "Identifier")
+		rID = fmt.Sprintf(protect(sha256Key, id))
+	case "Product_Win":
+		id, _ := readRegistr(`SOFTWARE\Microsoft\Windows NT\CurrentVersion`, "ProductId")
+		rID = fmt.Sprintf(protect(sha256Key, id))
+	default:
+		id := fmt.Sprintf("Error: %s is undefined command", in)
+		rID = id
+	}
+	return rID
+}
+
 //export RVExtension
 func RVExtension(output *C.char, outputsize C.size_t, input *C.char) {
-	id, _ := HardwareID()
-	temp := fmt.Sprintf("%s", id)
+	id := ReturnMyData(input)
+	temp := (fmt.Sprintf("%s", id))
 	// Return a result to Arma
 	result := C.CString(temp)
 	defer C.free(unsafe.Pointer(result))
