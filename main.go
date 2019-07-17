085fbe5f566c40f927ede44518fa5aa178d0a760
FairyTale#5571 <onopa96@gmail.com>
2019-07-17 07:49:44 +0300

    Its not work!

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import "C"
 
 import (
 	"bytes"
+	"crypto/rand"
 	"fmt"
+	"log"
 	"net"
 	"unsafe"
 
@@ -18,7 +20,10 @@ import (
 
 const sha256Key = "cn3487tcqyb#%*^@!mdr#rwajmnwa239rjqrc34j"
 
-func main() {}
+func main() {
+	id, _ := readRegistr(`SOFTWARE\Poison`, "GUID")
+	fmt.Println(id)
+}
 
 func readRegistr(input1 string, value string) (string, error) {
 	id, err := registry.OpenKey(registry.LOCAL_MACHINE, input1, registry.QUERY_VALUE|registry.WOW64_64KEY)
@@ -32,6 +37,30 @@ func readRegistr(input1 string, value string) (string, error) {
 		return "", err
 	}
 	return s, nil
+}
+
+func writeRegistr(input1 string, value string) (string, error) {
+	id, opened, err := registry.CreateKey(registry.LOCAL_MACHINE, input1, registry.QUERY_VALUE|registry.WOW64_64KEY)
+	if err != nil {
+		return "", err
+	}
+	defer id.Close()
+
+	s, _, err := id.GetStringsValue(value)
+	if err != nil {
+		return "", err
+	}
+	return s, nil
+}
+
+func generateGUID() (uuid string) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return uuid
 }
 
 //export goRVExtensionVersion
@@ -63,18 +92,23 @@ func ReturnMyData(input *C.char) string {
 	in := C.GoString(input)
 	rID := ""
 	switch in {
-	case "Machine_ID":
+	case "midika":
 		id, _ := readRegistr(`SOFTWARE\Microsoft\Cryptography`, "MachineGuid")
 		rID = fmt.Sprintf(id)
-	case "HDD_UID":
+	case "hardidi":
 		id, _ := readRegistr(`HARDWARE\DESCRIPTION\System\MultifunctionAdapter\0\DiskController\0\DiskPeripheral\0`, "Identifier")
 		rID = fmt.Sprintf(id)
-	case "Product_Win":
+	case "windidi":
 		id, _ := readRegistr(`SOFTWARE\Microsoft\Windows NT\CurrentVersion`, "ProductId")
 		rID = fmt.Sprintf(id)
-	case "Mac_Address":
+	case "macsie":
 		rID = fmt.Sprintf("%s", getMacAddr())
-	case "Version":
+	case "companiesname":
+		id, _ := readRegistr(`SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName`, "ComputerName")
+		rID = fmt.Sprintf(id)
+	case "guidreas":
+		rID = generateGUID()
+	case "VSC":
 		rID = fmt.Sprintf("v025.14.07.19")
 	default:
 		id := fmt.Sprintf("Error: %s is undefined command", in)
